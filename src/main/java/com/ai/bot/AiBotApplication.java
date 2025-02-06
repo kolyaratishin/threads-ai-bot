a40232ada83b657ace754531ebacc118fbb49230
Mykola Ratyshyn <mykola.ratyshyn@eleks.com>
2025-02-06 13:08:02 +0200

    project init

---
+++ src/main/java/com/ai/bot/AiBotApplication.java
@@ -0,0 +1,13 @@
+package com.ai.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiBotApplication.class, args);
+	}
+
+}
